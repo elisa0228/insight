8abf0c3fe6d6da1d2aae1ae0852043ac77d2f920
elisa0228 <elisasita@btinternet.com>
2026-01-21 13:34:33 +0000

    updated the pubspec.yaml page with three new packages to build the chat UI and integrating google gemini chat and allowing to pick images from the gallery

--- insight_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ insight_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import gal
+import path_provider_foundation
+import sqflite_darwin
+import url_launcher_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
