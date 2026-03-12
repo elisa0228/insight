00ad3ed9b1b1cac5fc3803073dc5340555c511f0
elisa0228 <elisasita@btinternet.com>
2026-03-12 12:20:40 +0000

    added a new 'text-to-speech' page to trial if the code works before implementing on 'chat_gemini' page

---
+++ insight_app/lib/pages/speech.dart
@@ -0,0 +1,2 @@
+String TTS_INPUT =
+    "This app is called Insight, a visual companion for the visually impaired.";
---
+++ insight_app/lib/pages/speech_integration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'; //core flutter UI framework for building cross-platform widgets
+
+class SpeechIntegration extends StatefulWidget {
+  const SpeechIntegration({super.key});
+
+  @override
+  State<SpeechIntegration> createState() => _SpeechIntegrationState();
+}
+
+class _SpeechIntegrationState extends State<SpeechIntegration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
